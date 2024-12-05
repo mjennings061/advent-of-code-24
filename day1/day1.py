15e9276a103b9e515d14d85afad53b0f60ba20ca
Michael Jennings <michael.jennings@b-secur.com>
2024-12-05 12:07:37 +0000

    Add similarity_score function to calculate prevalence in second column

--- day1/day1.py
+++ day1/day1.py
@@ -24,6 +24,29 @@ def pair_and_sort(unsorted: np.array) -> int:
     return sum_of_diff
 
 
+def similarity_score(unsorted: np.array) -> int:
+    """Count number of times first column value
+    appears in second column and sum.
+
+    Args:
+        unsorted (np.array): Array to process.
+
+    Returns:
+        sum_of_counts (int): Sum of appearences in second column."""
+    # Get index and search data columns.
+    index = unsorted[:, 0]
+    search_data = unsorted[:, 1]
+
+    # Search for each index in search data.
+    # Count prevalence and multiply by index.
+    # Return sum of prevalence.
+    sum_of_counts = 0
+    for value in index:
+        prevalence = np.sum(search_data == value)
+        sum_of_counts += value * prevalence
+    return sum_of_counts
+
+
 if __name__ == "__main__":
     # Input data.
     FILENAME = "day1.txt"
@@ -34,5 +57,10 @@ if __name__ == "__main__":
     with open(filepath, 'r') as file:
         input_array = np.loadtxt(file, dtype=int)
 
+    # Part one: pair and sort lists.
     result = pair_and_sort(input_array)
     print(f"Sum of differences: {result}")
+
+    # Part two: get similarity score.
+    similarity = similarity_score(input_array)
+    print(f"Similarity score: {similarity}")
